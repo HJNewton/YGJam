894dd210845b7e5a7c43309122d77311f29c27f8
Harry <harry3.newton@live.uwe.ac.uk>
2020-12-05 02:06:32 +0000

    Layout getting along nicely

---
+++ YGJam/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    [Header("Player Rotation Settings")]
+    public float mouseSensitivity = 100f;
+    public Transform playerBody;
+    float mouseX;
+    float mouseY;
+    float xRotation;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        Rotation();
+    }
+
+    void Rotation()
+    {
+        mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime; //Rotation on the X axis
+        mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime; //Rotation on the Y axis
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ YGJam/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Player Movement Settings")]
+    public float movementSpeed = 10f;
+    private Vector3 movementInput;
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        Vector3 playerForward = transform.forward;
+        Vector3 playerRight = transform.right; 
+
+        movementInput = new Vector3(-Input.GetAxisRaw("Vertical"), 0, Input.GetAxisRaw("Horizontal")); // Get inputs
+
+        Vector3 moveDir = (playerRight * movementInput.z + -playerForward * movementInput.x); // Apply inputs based on player facing direction
+
+        rb.velocity = moveDir.normalized * movementSpeed * Time.deltaTime; // Apply movement
+    }
+}
